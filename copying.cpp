156d6d15016b712bdbf4a25b82b21074fe54442b
davidjcmar <davidjcmar@gmail.com>
2016-02-02 16:41:08 -0600

    finito

--- copying.cpp
+++ copying.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
 #include <fstream>
 #include <string>
 #include <cstring>
@@ -14,7 +16,8 @@ using namespace std;
 
 int main (void)
 {
-	int times[11][2];
+	int byte_num;
+	int fd_in, fd_out;
 	char* buffer;
 	FILE* infile;
 	FILE* outfile;
@@ -27,44 +30,72 @@ int main (void)
 	{
 		stringstream convert_itoa;
 		string gen_f;
+		string make_file;
 		convert_itoa<<i;
 		gen_f = gen_file + convert_itoa.str() + " of=" + convert_itoa.str() + " >/dev/null 2>&1";
 
 		system(gen_f.c_str()); // system call to dd to quietly generate i * KB (1024) bits quietly
 		gen_f = convert_itoa.str();
-
-		/* set dynamic values*/
-		infile = fopen(gen_f.c_str(), "r");
-		gen_f += "_out";
-		outfile = fopen(gen_f.c_str(), "w");
-		buffer = (char*)malloc(sizeof(char) * (i*KB)+1);
+		
 		/* system call */
-
-		/* library call */
+		buffer = (char*)malloc(sizeof(char) * (i*KB));
+		fd_in = open(gen_f.c_str(), O_RDONLY);
+		gen_f += "_out";
+		make_file = "touch " + gen_f;
+		system(make_file.c_str());
+		fd_out = open(gen_f.c_str(), O_WRONLY);
+		
 		if (clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start_time) == 0)
-		{	
-			fread(buffer, 1, (i*KB)+1, infile);
-			fwrite(buffer, 1, (i*KB)+1, outfile);
+		{
+			byte_num = read(fd_in, buffer, (i*KB));
+			byte_num=write(fd_out, buffer, byte_num);
 			clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop_time);
 			elapse_time = stop_time.tv_sec - start_time.tv_sec;
 			elapse_time += ((stop_time.tv_nsec/1E9) - (start_time.tv_nsec/1E9));
-			cout<<i*KB<<"->"<<elapse_time<<"\n"; // output elapsed time
+			cout<<"sys: "<<byte_num<<"->"<<elapse_time<<"\n"; // output elapsed time
 		}
 		else
 		{
 			cout<<"ERROR: Clock failed.\n";
 			return 1;
 		}
-		/* testing */
-		//cout<<"buffer size: "<<sizeof(buffer)<<" -- "<<strlen(buffer)<<"\n";
-		//cout<<buffer<<"\n";
-		/* free allocated buffer */
+		free (buffer);
+		close (fd_in);
+		close (fd_out);
+		
+		/* init files and buffer*/
+		infile = fopen(gen_f.c_str(), "r");
+		//gen_f += "_out";
+		outfile = fopen(gen_f.c_str(), "w");
+		buffer = (char*)malloc(sizeof(char) * (i*KB));
+	
 
+
+
+		/* reinit files and buffer */
+		gen_f = convert_itoa.str();
+		gen_f += "_fout";
+		outfile = fopen(gen_f.c_str(), "w"); //open output file
+		//memset(buffer,'\n', (i*KB)); // init buffer
+		/* library call */
+		if (clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start_time) == 0)
+		{	
+			fread(buffer, 1, (i*KB), infile);
+			fwrite(buffer, 1, (i*KB), outfile);
+			clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop_time);
+			elapse_time = stop_time.tv_sec - start_time.tv_sec;
+			elapse_time += ((stop_time.tv_nsec/1E9) - (start_time.tv_nsec/1E9));
+			cout<<"lib: "<<i*KB<<"->"<<elapse_time<<"\n"; // output elapsed time
+		}
+		else
+		{
+			cout<<"ERROR: Clock failed.\n";
+			return 1;
+		}
 		/* cleanup */
+		free(buffer);
 		fclose(infile);
 		fclose(outfile);
-		free(buffer);
-		//system("rm test");
 	}
 	return 0;
 }
